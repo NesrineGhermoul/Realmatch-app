23d35603bfa9d52e397c4bdb7109ad40a72a046e
NesrineGhermoul <NesrineGhermoul@Gmail.com>
2025-12-01 22:52:01 -0700

    VideoBubble.js

---
+++ Frontend/frontend/src/components/VideoBubble.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function VideoBubble({ video }) {
+  return (
+    <View>
+      <Text>Video Placeholder</Text>
+    </View>
+  );
+}
