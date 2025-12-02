0954a4392967b24a5f7def98afb57a767f5e3d58
NesrineGhermoul <NesrineGhermoul@Gmail.com>
2025-12-01 22:19:34 -0700

    LiveModeScreen.js

---
+++ Frontend/LiveModeScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function LiveModeScreen() {
+  return (
+    <View>
+      <Text>Live Mode</Text>
+    </View>
+  );
+}
